573d4ce571167a2e9310de61759baca17f9c0bf3
Mathew Abraham <Abyonline@msn.com>
2025-09-19 10:31:28 -0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'calculator-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
